192eb1b197ea3707e89bf6fd56d00fcc03d2f79f
lukszeyan <s2012092@LKCSS.EDU.HK>
2016-11-19 12:38:14 +0800

    listitem1

---
+++ AnimeApp/app/src/main/java/com/example/s2012012/animeapp/listitem.java
@@ -0,0 +1,16 @@
+package com.example.s2012012.animeapp;
+
+/**
+ * Created by s2012092 on 19/11/2016.
+ */
+public class listitem {
+    private String title;
+    private String desc;
+    private String date;
+
+    public listitem(int type, String name, String time) {
+        this.title = title;
+        this.desc = desc;
+        this.date = date;
+    }
+}
